90969c5e801b56fc8725faa4434431febdadf36c
Ci Leong Ong <ongcileong2001@gmail.com>
2022-03-05 20:09:46 +0800

    Add exercise 4.1 attempt

--- Labs/week01/week01/exercise4.1.c
+++ Labs/week01/week01/exercise4.1.c
@@ -1,14 +1,22 @@
-//
-//  main.c
-//  week01
-//
-//  Created by Ci Leong Ong on 05/03/2022.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int prompt_int(char prompt[]) {
+    printf("%s", prompt);
+    int user_input = 0;
+    scanf("%d", &user_input);
+    return user_input;
+}
+
+int main() {
+    int input_value = prompt_int("Input the number of seconds: ");
+    
+    int hours = input_value / 3600;
+    int minutes = (input_value % 3600) / 60;
+    int seconds = input_value % 60;
+    
+    printf("%d seconds is equivalent to:\n", input_value);
+    printf("%d hours, %d minutes, and %d seconds\n",
+        hours, minutes, seconds);
+    
     return 0;
 }
