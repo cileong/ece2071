e6ca02c762f8936ea2ec1e14122e489836f203a5
Ci Leong Ong <ongcileong2001@gmail.com>
2022-03-05 19:57:58 +0800

    Add lab 1 base code

---
+++ Labs/week01/week01/exercise4.1.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  week01
+//
+//  Created by Ci Leong Ong on 05/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Labs/week01/week01/exercise4.2.c
@@ -0,0 +1,8 @@
+//
+//  exercise4.2.c
+//  week01
+//
+//  Created by Ci Leong Ong on 05/03/2022.
+//
+
+#include <stdio.h>
